30617bc6390bd994022071d6e308c48338cdcd51
Ben Y. <bjy777101@gmail.com>
2026-02-23 13:18:35 +1100

    [FEAT] : Switched to more conservative powersaving on BAT

--- nixpad/services/sysservices.nix
+++ nixpad/services/sysservices.nix
@@ -9,7 +9,13 @@
             enable = true;
             settings = {
                 CPU_SCALING_GOVERNOR_ON_AC = "performance";
-                CPU_SCALING_GOVERNOR_ON_BAT = "performance";
+                CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+                CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+                CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+                CPU_BOOST_ON_AC = 0;
+                CPU_BOOST_ON_BAT = 0;
 
                 START_CHARGE_THRESH_BAT0 = 75;
                 STOP_CHARGE_THRESH_BAT0 = 80;
