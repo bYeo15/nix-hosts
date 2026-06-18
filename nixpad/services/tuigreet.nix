3d1e4135127c0e2daa3dac4be0ef75083f673afc
Ben Y. <bjy777101@gmail.com>
2026-06-18 15:20:49 +1000

    [FIX] : (hopefully) fixes weirdness w/ greetd config handling of newlines

--- nixpad/services/tuigreet.nix
+++ nixpad/services/tuigreet.nix
@@ -4,18 +4,15 @@
     services.greetd = {
         enable = true;
         settings = {
-            default_session.command = ''
-                ${pkgs.tuigreet}/bin/tuigreet \
-                --time \
-                --asterisks \
-                --user-menu \
-                --cmd ${pkgs.bash}/bin/bash
-            '';
+            # fix for argument bug (prevents errors for newline in config)
+            # TODO : this will probably be broken again and/or unnecessary when fixed
+            default_session.command = lib.join " " [
+                (lib.getExe pkgs.tuigreet)
+                "--time"
+                "--asterisks"
+                "--user-menu"
+                "--cmd ${lib.getExe pkgs.bash}"
+            ];
         };
     };
-
-    environment.etc."greetd/environments".text = ''
-        bash
-        sway
-    '';
 }
